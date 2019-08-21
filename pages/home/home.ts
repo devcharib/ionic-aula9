6af4fb7cdd77be77e6745edc2be71e0d6e6832d0
devcharib <charllyribeiro@gmail.com>
2019-08-21 20:42:16 -0300

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AlertController} from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import usuarios from './usuarios.json';
 
 @Component({
